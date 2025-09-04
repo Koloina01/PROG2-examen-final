adb164d14f95d6e2e300f3760e0e766901b7c209
Koloina01 <kolomaherison@gmail.com>
2025-09-04 10:04:40 +0300

    feat: ajouter Examen.java

--- src/main/java/Examen.java
+++ src/main/java/Examen.java
@@ -1,16 +1,30 @@
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class Examens {
+@EqualsAndHashCode
+public class Examen {
     private int id;
     private String title;
     private Cours coursRattache;
     private Instant dateEtHeureExamen;
     private int coefficient;
+    private List<Notes> notes;
+
+    public double getExamGrade(Examen examen, Etudiant etudiant, Instant t) {
+        for(Notes note : this.notes) {
+            if(note.getEtudiant().equals(etudiant)) {
+
+            }
+        }
+    }
+
 }
---
+++ src/test/java/GetExamGradeTest.java
@@ -0,0 +1,6 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
